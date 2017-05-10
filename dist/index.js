3f68464aff92f8f5a940e53870f96892d011140f
Rylius <ryliusk@gmail.com>
2017-05-10 13:16:53 +0200

    Changed build system to rollup

--- dist/index.js
+++ dist/index.js
@@ -1,163 +1,102 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(['exports', '../src/state', '../src/update'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(exports, require('../src/state'), require('../src/update'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(mod.exports, global.state, global.update);
-        global.index = mod.exports;
-    }
-})(this, function (exports, _state, _update) {
-    'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global['resistopia-reactor-simulation'] = factory());
+}(this, (function () { 'use strict';
 
-    Object.defineProperty(exports, "__esModule", {
-        value: true
-    });
-
-    var _update2 = _interopRequireDefault(_update);
-
-    function _interopRequireDefault(obj) {
-        return obj && obj.__esModule ? obj : {
-            default: obj
-        };
-    }
-
-    exports.default = {
-        createInitialState: _state.createInitialState,
-        update: _update2.default
+function createInitialState(program) {
+    var state = {
+        tick: 0,
+        seconds: 0,
+        time: Date.now(),
+        stateMachines: {},
+        inputs: {}
     };
-});
-(function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(["exports"], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(exports);
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(mod.exports);
-        global.state = mod.exports;
-    }
-})(this, function (exports) {
-    "use strict";
 
-    Object.defineProperty(exports, "__esModule", {
-        value: true
-    });
-    exports.createInitialState = createInitialState;
-    exports.getInput = getInput;
-    function createInitialState(program) {
-        var state = {
-            tick: 0,
-            seconds: 0,
-            time: Date.now(),
-            stateMachines: {},
-            inputs: {}
-        };
-
-        program.stateMachines.forEach(function (stateMachine) {
-            if (stateMachine.initialState) {
-                state.stateMachines[stateMachine.id] = stateMachine.initialState();
-            } else {
-                state.stateMachines[stateMachine.id] = {};
-            }
-
-            state.inputs[stateMachine.id] = getInput(stateMachine, state, 0);
-        });
-
-        return state;
-    }
-
-    function getInput(stateMachine, prevState) {
-        if (!stateMachine.input) {
-            return {};
-        }
-
-        return stateMachine.input(prevState);
-    }
-});
-(function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(['exports', './state'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(exports, require('./state'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(mod.exports, global.state);
-        global.update = mod.exports;
-    }
-})(this, function (exports, _state) {
-    'use strict';
-
-    Object.defineProperty(exports, "__esModule", {
-        value: true
-    });
-    exports.default = update;
-
-
-    function parseInput(prevState, state, stateMachine) {
-        var input = {};
-
-        var inputSources = state.inputs[stateMachine.id];
-        Object.keys(inputSources).forEach(function (sourceId) {
-            var sourceState = state.stateMachines[sourceId] || prevState.stateMachines[sourceId];
-            parseInputSource(sourceState, inputSources[sourceId], input, stateMachine.id, sourceId);
-        });
-
-        return input;
-    }
-
-    function parseInputSource(sourceState, source, input, parentId, sourceId) {
-        var targetProperty = source.as || source.property;
-        var max = typeof source.max === 'number' ? source.max : sourceState[source.property];
-        var value = Math.min(sourceState[source.property], max);
-
-        console.log(parentId + '.' + targetProperty + ': ' + value + ' from ' + sourceId + '.' + source.property + ' (' + sourceState[source.property] + ')');
-
-        if (!source.readOnly) {
-            sourceState[source.property] -= value;
-        }
-        input[targetProperty] = value;
-    }
-
-    function update(program, prevState) {
-        var state = {
-            tick: prevState.tick + 1,
-            time: Date.now(),
-            stateMachines: {},
-            inputs: {}
-        };
-
-        program.stateMachines.forEach(function (stateMachine) {
-            state.inputs[stateMachine.id] = (0, _state.getInput)(stateMachine, prevState.stateMachines[stateMachine.id]);
-        });
-
-        program.stateMachines.forEach(function (stateMachine) {
+    program.stateMachines.forEach(function (stateMachine) {
+        if (stateMachine.initialState) {
+            state.stateMachines[stateMachine.id] = stateMachine.initialState();
+        } else {
             state.stateMachines[stateMachine.id] = {};
+        }
 
-            if (!stateMachine.update) {
-                return;
-            }
+        state.inputs[stateMachine.id] = getInput(stateMachine, state, 0);
+    });
 
-            var input = parseInput(prevState, state, stateMachine);
+    return state;
+}
 
-            state.stateMachines[stateMachine.id] = stateMachine.update(prevState.stateMachines[stateMachine.id], input);
-        });
+function getInput(stateMachine, prevState) {
+    if (!stateMachine.input) {
+        return {};
+    }
 
-        // console.log(`tick ${state.tick}`);
-        // console.log(state.inputs);
-        // console.log('-----------------');
-        // console.log(state.stateMachines);
-        // console.log('=================');
+    return stateMachine.input(prevState);
+}
 
-        return state;
+function parseInput(prevState, state, stateMachine) {
+    var input = {};
+
+    var inputSources = state.inputs[stateMachine.id];
+    Object.keys(inputSources).forEach(function (sourceId) {
+        var sourceState = state.stateMachines[sourceId] || prevState.stateMachines[sourceId];
+        parseInputSource(sourceState, inputSources[sourceId], input, stateMachine.id, sourceId);
+    });
+
+    return input;
+}
+
+function parseInputSource(sourceState, source, input, parentId, sourceId) {
+    var targetProperty = source.as || source.property;
+    var max = typeof source.max === 'number' ? source.max : sourceState[source.property];
+    var value = Math.min(sourceState[source.property], max);
+
+    console.log(parentId + '.' + targetProperty + ': ' + value + ' from ' + sourceId + '.' + source.property + ' (' + sourceState[source.property] + ')');
+
+    if (!source.readOnly) {
+        sourceState[source.property] -= value;
+    }
+    input[targetProperty] = value;
+}
+
+function update(program, prevState) {
+    var state = {
+        tick: prevState.tick + 1,
+        time: Date.now(),
+        stateMachines: {},
+        inputs: {}
     };
-});
 
-//# sourceMappingURL=index.js.map
+    program.stateMachines.forEach(function (stateMachine) {
+        state.inputs[stateMachine.id] = getInput(stateMachine, prevState.stateMachines[stateMachine.id]);
+    });
+
+    program.stateMachines.forEach(function (stateMachine) {
+        state.stateMachines[stateMachine.id] = {};
+
+        if (!stateMachine.update) {
+            return;
+        }
+
+        var input = parseInput(prevState, state, stateMachine);
+
+        state.stateMachines[stateMachine.id] = stateMachine.update(prevState.stateMachines[stateMachine.id], input);
+    });
+
+    // console.log(`tick ${state.tick}`);
+    // console.log(state.inputs);
+    // console.log('-----------------');
+    // console.log(state.stateMachines);
+    // console.log('=================');
+
+    return state;
+}
+
+var index = {
+    createInitialState: createInitialState,
+    update: update
+};
+
+return index;
+
+})));
