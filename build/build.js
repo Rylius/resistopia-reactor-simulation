48bed9284833b1a23f3903c0e8f7a7f3bbcb06de
Rylius <ryliusk@gmail.com>
2017-05-10 13:42:39 +0200

    Fixed build/unit tests using incorrect babel preset

--- build/build.js
+++ build/build.js
@@ -3,7 +3,7 @@ const babel = require('rollup-plugin-babel');
 
 rollup.rollup({
     entry: 'src/index.js',
-    plugins: [babel()],
+    plugins: [babel({babelrc: false, presets: ['babel-preset-es2015-rollup']})],
 }).then(function (bundle) {
     bundle.write({
         dest: 'dist/index.js',
