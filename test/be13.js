7031546b58a2056fa9e7b42cb13d1b36bdce472a
Rylius <ryliusk@gmail.com>
2017-08-23 00:02:03 +0200

    Increased energy-capacitor size

--- test/be13.js
+++ test/be13.js
@@ -6,11 +6,11 @@ import be13 from '../data/be13';
 const program = be13();
 
 let state = createInitialState(program);
-state.stateMachines['storage-matter'].releasedMatterPerTick = 500;
-state.stateMachines['storage-antimatter'].releasedAntimatterPerTick = 500;
-state.stateMachines['energy-converter'].energyConversion = 100;
-state.stateMachines['reactor-cooling'].cooling = 150;
-for (let i = 0; i < 20; i++) {
+// state.stateMachines['storage-matter'].releasedMatterPerTick = 120;
+// state.stateMachines['storage-antimatter'].releasedAntimatterPerTick = 120;
+// state.stateMachines['energy-converter'].energyConversion = 100;
+state.stateMachines['reactor-cooling'].cooling = 0.5;
+for (let i = 0; i < 3200; i++) {
     state = update(program, state);
     console.log('--------------------');
     console.log(state);
